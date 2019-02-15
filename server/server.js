9ea9f3420e08f1c9591c03dd701bf2e6c02d331e
Yannis Alexiou <johnmast7@yahoo.com>
2019-02-15 16:01:51 +0200

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -10,11 +12,34 @@ const port = process.env.PORT || 3000;
 //We dont configure express by passing arguments
 //We configure express by calling methods on app
 var app = express();
+//When we call app.listen method we actually call the below same method
+var server = http.createServer(app);
+//We get back our web socket.
+//On here we can do everything we want as emmiting or listening on events
+//This is how we communicate between server and client
+var io = socketIO(server);
+//After creating the above var we are ready to receive connections
+//So we need to build one from the client
 
 //To configure our express static middleware
 //This help ass to show on localhost our index.html page
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//let us register an event listener
+//we can listen for a specific event
+//And do something when the event happens
+//This let us listen for a new connection (client -> server)
+//The socket argument is very similar to the socket argument
+//from client (html file)
+//This represents the individual socket to seperate from other (clients)
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 });
